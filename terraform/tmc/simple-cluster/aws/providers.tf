27c6ceeaaaa9975f9c74059aefbe8fcdbcc10fbb
Niall Thomson <nthomson@pivotal.io>
2020-03-26 21:41:50 -0700

    Move providers to file

---
+++ terraform/tmc/simple-cluster/aws/providers.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = var.region
+}
+
+provider "kubernetes" {
+  host                   = data.tmc_kubeconfig.kubeconfig.server
+
+  client_certificate     = base64decode(data.tmc_kubeconfig.kubeconfig.client_certificate)
+  client_key             = base64decode(data.tmc_kubeconfig.kubeconfig.client_key)
+  cluster_ca_certificate = base64decode(data.tmc_kubeconfig.kubeconfig.cluster_ca_certificate)
+
+  load_config_file       = false
+}
+
+provider "k14s" {
+  kubeconfig_yml = data.tmc_kubeconfig.kubeconfig.content
+}
