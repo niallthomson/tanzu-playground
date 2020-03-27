47cd2af47ebf4b7b14ea707a39fda0cff0fd35c1
Niall Thomson <nthomson@pivotal.io>
2020-03-27 00:12:01 -0700

    Commented outputs

--- terraform/gke/cf-for-k8s/outputs.tf
+++ terraform/gke/cf-for-k8s/outputs.tf
@@ -1,11 +1,14 @@
 output "cf_api_endpoint" {
-  value = module.cf-for-k8s.cf_api_endpoint
+  description = "Cloud Foundry API endpoint"
+  value       = module.cf-for-k8s.cf_api_endpoint
 }
 
 output "cf_admin_username" {
-  value = module.cf-for-k8s.cf_admin_username
+  description = "Cloud Foundry admin username"
+  value       = module.cf-for-k8s.cf_admin_username
 }
 
 output "cf_admin_password" {
-  value = module.cf-for-k8s.cf_admin_password
+  description = "Cloud Foundry admin password"
+  value       = module.cf-for-k8s.cf_admin_password
 }
